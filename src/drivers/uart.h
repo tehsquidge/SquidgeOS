98f7ce6f1e0761066c40db1da6751ee8a0ff4e19
Liam Kerr <liam.d.kerr@protonmail.com>
2026-02-10 02:52:43 +0000

    Cleaning up #defines from bad approach

--- src/drivers/uart.h
+++ src/drivers/uart.h
@@ -4,10 +4,6 @@
 #define UART_RBR (volatile uint8_t *)(UART_ADDRESS + 0)
 #define UART_LSR (volatile uint8_t *)(UART_ADDRESS + 5)
 #define UART_ADDRESS 0x10000000
-#define UART_INTERRUPT_ENABLE_REGISTER 0x1001100C
-#define UART_INTEN_OFFSET 0x0C
-#define UART_TX_OFFSET 0x04
-#define UART_INTERRUPT_ENABLE_REGISTER (UART_BASE_ADDR + UART_INTEN_OFFSET)
 
 void uart_init();
 void uart_put(size_t base_addr, uint8_t data);
